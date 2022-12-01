9499ce2b207040ea7a0ed35d2a8e25ee7c042eca
Alhajideen <salifunurudeen834@gmail.com>
2022-12-01 17:07:19 -0600

    basic routing with express

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server');
+  res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('You can post here');
 });
 
 const port = 5000;
