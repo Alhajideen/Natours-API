90604d4626fc6bfed78a692214a1f4dc9eb7e53c
Alhajideen <salifunurudeen834@gmail.com>
2022-12-10 16:28:29 -0600

    move server scripts to server.js file

---
+++ routes/tourRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkId,
+} = require('../controllers/tourController');
+
+const router = express.Router();
+
+router.param('id', checkId);
+
+// Methods/Controllers
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router;
